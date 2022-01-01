63a1a9128640c157192152105ad39fa55c680087
niludarekar1 <nilu.darekar1@gmail.com>
2022-01-01 19:25:41 +0530

    MODIFIED: Employee Daily Wages Calculated

--- src/com/bridgelaz/EmpWeges/EmployeeWeges.java
+++ src/com/bridgelaz/EmpWeges/EmployeeWeges.java
@@ -2,15 +2,23 @@ package com.bridgelaz.EmpWeges;
 
 public class EmployeeWeges {
 
+    private static int IS_PRESENT = 1;
+    //private static  IS_PRESENT = 1;
+    private static int WAGE_PER_HOUR =20;
+    private static int FULL_DAY_HOUR=8;
     public static void main(String[] args) {
-      int IS_PRESENT =1;
+
 
       int  eCheck = (int) Math.floor(Math.random() * 10) % 2; //0.0 -1.0
 
         //System.out.println(eCheck);
+        int eWage=0;
 
         if (eCheck == IS_PRESENT){
             System.out.println("Employee is Present");
+            eWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+            System.out.println("Employee Wages Details :- "+eWage);
+
     }
         else {
             System.out.println("Employee is Absent");
