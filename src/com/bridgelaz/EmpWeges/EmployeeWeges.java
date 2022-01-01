a8905e5cc35371e78359cb0a3ee8f460bca77cee
niludarekar1 <nilu.darekar1@gmail.com>
2022-01-01 20:27:51 +0530

    MODIFIED : Wages Calculated as per Time Basis

--- src/com/bridgelaz/EmpWeges/EmployeeWeges.java
+++ src/com/bridgelaz/EmpWeges/EmployeeWeges.java
@@ -3,9 +3,11 @@ package com.bridgelaz.EmpWeges;
 public class EmployeeWeges {
 
     private static int IS_PRESENT = 1;
-    //private static  IS_PRESENT = 1;
     private static int WAGE_PER_HOUR =20;
-    private static int FULL_DAY_HOUR=8;
+//    private static int FULL_DAY_HOUR=8;
+    private static int IS_PART_TIME=1;
+    private static int IS_FULL_TIME=2;
+
     public static void main(String[] args) {
 
 
@@ -13,15 +15,20 @@ public class EmployeeWeges {
 
         //System.out.println(eCheck);
         int eWage=0;
+        int eHrs=0;
 
         if (eCheck == IS_PRESENT){
-            System.out.println("Employee is Present");
-            eWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-            System.out.println("Employee Wages Details :- "+eWage);
-
-    }
-        else {
+            eHrs=4;
+           }
+        else if(eCheck == IS_FULL_TIME) {
+            eHrs = 8;
+        }
+        else
+        {
+            eHrs=0;
             System.out.println("Employee is Absent");
         }
+         eWage= WAGE_PER_HOUR*eHrs;
+        System.out.println("Employee Wages Details :- "+eWage);
     }
 }
