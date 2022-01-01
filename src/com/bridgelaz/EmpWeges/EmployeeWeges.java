6c9b9eb3fd1578e545189eac49ecd7a49a0f9fd1
niludarekar1 <nilu.darekar1@gmail.com>
2022-01-01 21:37:36 +0530

    MODIFIED :Calculating Wages as Work Day In Month

--- src/com/bridgelaz/EmpWeges/EmployeeWeges.java
+++ src/com/bridgelaz/EmpWeges/EmployeeWeges.java
@@ -7,30 +7,38 @@ public class EmployeeWeges {
 //    private static int FULL_DAY_HOUR=8;
     private static  final int  IS_PART_TIME=1;
     private static  final int IS_FULL_TIME=2;
+    private static final int WORK_DAY_MONTH=20;
 
     public static void main(String[] args) {
 
 
-      double eCheck = Math.floor(Math.random() * 10) % 3; //0.0 -1.0
+        int eWage = 0;
+        int eHrs = 0;
+        int totalEmpWages = 0;
 
-        //System.out.println(eCheck);
-        int eWage=0;
-        int eHrs=0;
+        for (int i = 1; i <= WORK_DAY_MONTH; i++) {
 
-         switch ((int)eCheck){
-             case IS_PART_TIME:
-                 System.out.println("Employee is Present on Part Time");
-                    eHrs=4;
-                  break;
-             case IS_FULL_TIME:
-                 System.out.println("Employee is Present on Full Time");
-                 eHrs=8;
-                 break;
-             default:
-                 System.out.println("Employee is Absent");
-                 eHrs=0;
-         }
-         eWage= WAGE_PER_HOUR*eHrs;
-        System.out.println("Employee Wages Details :- "+eWage);
+            double eCheck = Math.floor(Math.random() * 10) % 3; //0.0 -1.0
+
+            //System.out.println(eCheck);
+
+            switch ((int) eCheck) {
+                case IS_PART_TIME:
+                    System.out.println("Employee is Present on Part Time");
+                    eHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    System.out.println("Employee is Present on Full Time");
+                    eHrs = 8;
+                    break;
+                default:
+                    System.out.println("Employee is Absent");
+                    eHrs = 0;
+            }
+            eWage = WAGE_PER_HOUR * eHrs;
+            totalEmpWages= eWage+totalEmpWages;
+            System.out.println("Employee Wages Details :- " + eWage);
+        }
+        System.out.println("Total Employee Wages"+totalEmpWages);
     }
 }
